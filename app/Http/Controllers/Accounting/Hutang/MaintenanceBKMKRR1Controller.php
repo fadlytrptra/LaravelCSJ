40cd3380a1387a97d2aba215ac0591f891b133e8
christiantadam <63576449+christiantadam@users.noreply.github.com>
2026-02-26 11:04:05 +0700

    fix<Accounting>
    Implementasi fitur Maintenance BKM KRR 1

--- app/Http/Controllers/Accounting/Hutang/MaintenanceBKMKRR1Controller.php
+++ app/Http/Controllers/Accounting/Hutang/MaintenanceBKMKRR1Controller.php
@@ -48,7 +48,7 @@ class MaintenanceBKMKRR1Controller extends Controller
 
         if (!empty($request->id_bkm)) {
             DB::connection('ConnAccounting')
-                ->statement('EXEC SP_5298_ACC_INSERT_BKM_TPELUNASAN @idBKM = ?, @tglinput = ?, @userinput = ?, @terjemahan = ?, @nilaipelunasan = ?, @kode = ?', [
+                ->statement('EXEC SP_1273_PRG_INSERT_BKM_TPELUNASAN @idBKM = ?, @tglinput = ?, @userinput = ?, @terjemahan = ?, @nilaipelunasan = ?, @kode = ?', [
                     trim($request->id_bkm),
                     $request->tanggal_input,
                     trim(Auth::user()->NomorUser),
@@ -65,7 +65,7 @@ class MaintenanceBKMKRR1Controller extends Controller
                 $itemValue = $matches[1] ?? null;
                 // dd($itemValue);
                 DB::connection('ConnAccounting')
-                    ->statement('EXEC SP_5298_ACC_INSERT_BKM_TPELUNASAN_TAG_TUNAI @idBKM = ?, @tgl = ?, @idBank = ?, @idUang = ?, @idJenis = ?, @kodeperkiraan= ?, @uraian = ?, @keterangan = ?, @bukti = ?, @user = ?, @nilaipelunasan = ?, @kurs = ?', [
+                    ->statement('EXEC SP_1273_PRG_INSERT_BKM_TPELUNASAN_TAG_TUNAI @idBKM = ?, @tgl = ?, @idBank = ?, @idUang = ?, @idJenis = ?, @kodeperkiraan= ?, @uraian = ?, @keterangan = ?, @bukti = ?, @user = ?, @nilaipelunasan = ?, @kurs = ?', [
                         trim($request->id_bkm),
                         $request->tanggal_input,
                         $request->id_bank,
@@ -90,7 +90,7 @@ class MaintenanceBKMKRR1Controller extends Controller
                 // dd($ada)
                 if ($ada1 === "true") {
                     $idPelunasan = DB::connection('ConnAccounting')
-                        ->select('EXEC SP_5298_ACC_GET_IDPELUNASAN');
+                        ->select('EXEC SP_1273_PRG_GET_IDPELUNASAN');
                     $IDPelunasan = $idPelunasan[0]->id_pelunasan;
                     // dd($IDPelunasan);
                     // dd($idPelunasan);
@@ -101,7 +101,7 @@ class MaintenanceBKMKRR1Controller extends Controller
                             // dd($itemBiaya);
                             if ($itemValue == $biayaItem[3]) {
                                 DB::connection('ConnAccounting')
-                                    ->statement('EXEC SP_5298_ACC_INSERT_DETAIL_BIAYA @idpelunasan = ?, @keterangan = ?, @biaya = ?, @kodeperkiraan = ?', [
+                                    ->statement('EXEC SP_1273_PRG_INSERT_DETAIL_BIAYA @idpelunasan = ?, @keterangan = ?, @biaya = ?, @kodeperkiraan = ?', [
                                         $IDPelunasan,
                                         $itemBiaya,
                                         floatval(str_replace(",", "", $biayaItem[1])),
@@ -111,7 +111,7 @@ class MaintenanceBKMKRR1Controller extends Controller
                         }
                     } else {
                         DB::connection('ConnAccounting')
-                            ->statement('EXEC SP_5298_ACC_INSERT_DETAIL_BIAYA @idpelunasan = ?, @keterangan = ?, @biaya = ?, @kodeperkiraan = ?', [
+                            ->statement('EXEC SP_1273_PRG_INSERT_DETAIL_BIAYA @idpelunasan = ?, @keterangan = ?, @biaya = ?, @kodeperkiraan = ?', [
                                 $IDPelunasan,
                                 '',
                                 $biaya,
@@ -122,7 +122,7 @@ class MaintenanceBKMKRR1Controller extends Controller
             }
 
             DB::connection('ConnAccounting')
-                ->statement('EXEC SP_5298_ACC_UPDATE_COUNTER_IDBKM ?, ?, ?, ?', [
+                ->statement('EXEC SP_1273_PRG_UPDATE_COUNTER_IDBKM ?, ?, ?, ?', [
                     $idbkm,
                     $request->id_bank,
                     trim($request->jenis_bank),
@@ -141,7 +141,7 @@ class MaintenanceBKMKRR1Controller extends Controller
         if ($id == 'getKira') {
             // Execute the stored procedure
             $results = DB::connection('ConnAccounting')
-                ->select('exec SP_1273_ACC_LIST_BKK1_KODEPERKIRAAN');
+                ->select('exec SP_1273_PRG_LIST_BKK1_KODEPERKIRAAN');
             // dd($results);
             $response = [];
             foreach ($results as $row) {
@@ -154,7 +154,7 @@ class MaintenanceBKMKRR1Controller extends Controller
             return datatables($response)->make(true);
         } else if ($id == 'getMataUang') {
             $results = DB::connection('ConnAccounting')
-                ->select('exec SP_5298_ACC_LIST_MATA_UANG @kode = ?', [1]);
+                ->select('exec SP_1273_PRG_LIST_MATA_UANG @kode = ?', [1]);
             // dd($results);
             $response = [];
             foreach ($results as $row) {
@@ -166,9 +166,9 @@ class MaintenanceBKMKRR1Controller extends Controller
 
             return datatables($response)->make(true);
         } else if ($id == 'getBank') {
-            // Execute the stored procedure for 'SP_5298_ACC_LIST_BANK'
+            // Execute the stored procedure for 'SP_1273_PRG_LIST_BANK'
             $results = DB::connection('ConnAccounting')
-                ->select('exec SP_5298_ACC_LIST_BANK');
+                ->select('exec SP_1273_PRG_LIST_BANK');
             // dd($results);
             $response = [];
             foreach ($results as $row) {
@@ -180,10 +180,10 @@ class MaintenanceBKMKRR1Controller extends Controller
 
             return datatables($response)->make(true);
         } else if ($id == 'getBankDetail') {
-            // Execute the stored procedure for 'SP_5298_ACC_LIST_BANK_1'
+            // Execute the stored procedure for 'SP_1273_PRG_LIST_BANK_1'
             $bankId = $request->input('id_bank');
             $result = DB::connection('ConnAccounting')
-                ->select('exec SP_5298_ACC_LIST_BANK_1 @idBank = ?', [trim($bankId)]);
+                ->select('exec SP_1273_PRG_LIST_BANK_1 @idBank = ?', [trim($bankId)]);
             // dd($result);
             if (!empty($result)) {
                 $response = [
@@ -194,7 +194,7 @@ class MaintenanceBKMKRR1Controller extends Controller
         } else if ($id == 'getJenisBayar') {
             // Execute the stored procedure
             $results = DB::connection('ConnAccounting')
-                ->select('exec SP_5298_ACC_JENIS_DOK');
+                ->select('exec SP_1273_PRG_JENIS_DOK');
 
             $response = [];
             foreach ($results as $row) {
@@ -252,7 +252,7 @@ class MaintenanceBKMKRR1Controller extends Controller
             }
         } else if ($id == 'getPelunasanTunai') {
             $results = DB::connection('ConnAccounting')
-                ->select('exec SP_5298_ACC_LIST_PELUNASAN_TUNAI');
+                ->select('exec SP_1273_PRG_LIST_PELUNASAN_TUNAI');
             // dd($results);
             $response = [];
             $j = 0;
@@ -274,7 +274,7 @@ class MaintenanceBKMKRR1Controller extends Controller
             $tgl2 = $request->input('tgl_akhirbkk');
             // dd($tgl1, $tgl2);
             $results = DB::connection('ConnAccounting')
-                ->select('exec SP_5298_ACC_LIST_PELUNASAN_TUNAI_PERTGL ?, ?', [$tgl1, $tgl2]);
+                ->select('exec SP_1273_PRG_LIST_PELUNASAN_TUNAI_PERTGL ?, ?', [$tgl1, $tgl2]);
             // dd($results);
             $response = [];
             foreach ($results as $row) {
@@ -296,7 +296,7 @@ class MaintenanceBKMKRR1Controller extends Controller
 
 
             $results = DB::connection('ConnAccounting')
-                ->select('exec SP_5298_ACC_GET_FIELD_IDPELUNASAN @idBKM = ?', [trim($request->input('bkm'))]);
+                ->select('exec SP_1273_PRG_GET_FIELD_IDPELUNASAN @idBKM = ?', [trim($request->input('bkm'))]);
 
             foreach ($results as $row) {
                 $brs++;
@@ -311,7 +311,7 @@ class MaintenanceBKMKRR1Controller extends Controller
             if ($brs != 0) {
                 foreach ($idArray as $idPelunasan) {
                     $resultCount = DB::connection('ConnAccounting')
-                        ->select('exec SP_5298_ACC_COUNT_IDPELUNASAN @idpelunasan = ?', [$idPelunasan]);
+                        ->select('exec SP_1273_PRG_COUNT_IDPELUNASAN @idpelunasan = ?', [$idPelunasan]);
 
                     if (!empty($resultCount) && $resultCount[0]->ada > 0) {
                         $ada = true;
@@ -335,7 +335,7 @@ class MaintenanceBKMKRR1Controller extends Controller
             // Misalnya menggunakan library reporting atau mencetak langsung
 
             DB::connection('ConnAccounting')
-                ->statement('exec SP_5298_ACC_UPDATE_TGLCETAK_BKM @idBKM = ?', [trim($request->input('bkm'))]);
+                ->statement('exec SP_1273_PRG_UPDATE_TGLCETAK_BKM @idBKM = ?', [trim($request->input('bkm'))]);
             // dd($sno);
             return response()->json([
                 'data' => $sno,
